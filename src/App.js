b6d338f31ffff57c202778af8895af317cca4a80
BlackMonarch4643 <mishraudit5@gmail.com>
2023-08-05 20:30:19 +0530

    added a form and used state hook to perform uppercase and handled onchange and onclick events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/textform";
 
 function App() {
   return (
     <>
-    <Navbar title = "TextUtils" head1 = "Home" head2 = "About"/>
+      <Navbar title="TextUtils" head1="Home" head2="About" />
+      <div className="container my-3">
+        <TextForm heading="Enter Text to analyze below"/>
+      </div>
     </>
   );
 }
