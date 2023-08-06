81463d34df60413febc6b9feb1607021a6df7b73
BlackMonarch4643 <mishraudit5@gmail.com>
2023-08-06 09:25:11 +0530

    we have experimented with styles inline and used toggle style using onClick

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/textform";
+//import TextForm from "./components/textform";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" head1="Home" head2="About" />
-      <div className="container my-3">
+      {/* <div className="container my-3">
         <TextForm heading="Enter Text to analyze below"/>
-      </div>
+      </div> */}
+      <About />
     </>
   );
 }
