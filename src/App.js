b0115c5383cdb771f80f6bf4e66ca790797dff92
BlackMonarch4643 <mishraudit5@gmail.com>
2023-08-08 12:10:19 +0530

    updated dark and light mode on the entire page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-//import TextForm from "./components/textform";
-import About from "./components/About";
+import TextForm from "./components/textform";
+//import About from "./components/About";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+  let toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = 'rgb('+[33,37,41].join(',')+')';
+    }
+  };
   return (
     <>
-      <Navbar title="TextUtils" head1="Home" head2="About" />
-      {/* <div className="container my-3">
-        <TextForm heading="Enter Text to analyze below"/>
-      </div> */}
-      <About />
+      <Navbar
+        title="TextUtils"
+        head1="Home"
+        head2="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <div className="container my-3">
+        <TextForm heading="Enter Text to analyze below" mode={mode} />
+      </div>
+      {/* <About /> */}
     </>
   );
 }
--- src/components/textform.js
+++ src/components/textform.js
@@ -6,10 +6,10 @@ export default function TextForm(props) {
       let newtext = text.toUpperCase();
       setText(newtext);
       setCase("Convert to lowercase");
-    }else if(case1 === "Convert to lowercase"){
-        let newtext = text.toLowerCase();
-        setText(newtext);
-        setCase("Convert to uppercase");
+    } else if (case1 === "Convert to lowercase") {
+      let newtext = text.toLowerCase();
+      setText(newtext);
+      setCase("Convert to uppercase");
     }
   };
 
@@ -23,40 +23,43 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
   const [case1, setCase] = useState("Convert to uppercase");
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   return (
     <>
-      <div className="container">
-        <h1>{props.heading}</h1>
-        <form>
-          <div className="form-group">
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="8"
-              value={text}
-              onChange={handleOnChange}
-              onClick={handleTextClick}
-            ></textarea>
-          </div>
-        </form>
-        <button className="btn btn-primary my-3" onClick={handleClick}>
-          {" "}
-          {case1}
-        </button>
-      </div>
-      <div className="conatiner" my-2>
-        <h1>Text Summary</h1>
-        <p>
-          Your Entered text is of {text.split(" ").length} words and{" "}
-          {text.length} characters.
-        </p>
-        <p>
-          Slowest reader will take a max of {0.008 * text.split(" ").length}{" "}
-          minutes
-        </p>
-        <h2 className="my-3"> Preview </h2>
-        <p>{text}</p>
+      <div style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
+        <div className="container">
+          <h1>{props.heading}</h1>
+          <form>
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="8"
+                value={text}
+                onChange={handleOnChange}
+                onClick={handleTextClick}
+              ></textarea>
+            </div>
+          </form>
+          <button className="btn btn-primary my-3" onClick={handleClick}>
+            {" "}
+            {case1}
+          </button>
+        </div>
+        <div className="conatiner" my-2>
+          <h1>Text Summary</h1>
+          <p>
+            Your Entered text is of{" "}
+            {text.length > 0 ? text.split(" ").length : 0} words and{" "}
+            {text.length} characters.
+          </p>
+          <p>
+            Slowest reader will take a max of{" "}
+            {0.008 * text.length > 0 ? text.split(" ").length : 0} minutes
+          </p>
+          <h2 className="my-3"> Preview </h2>
+          <p>{text.length > 0 ? text : "Enter your text to preview it here"}</p>
+        </div>
       </div>
     </>
   );
